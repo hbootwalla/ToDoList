e5971dda6b805d3bd1cbcbf16649b21cc4a331cc
hbootwalla <hbootwalla@gmail.com>
2017-11-07 19:27:14 -0500

    First Commit

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+$("ul").on("click", "li", function(){
+    console.log("workes");
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click","span", function(event){
+    $(this).parent().fadeOut(1000, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    let EnterKeyCode = 13;
+    if(event.which === EnterKeyCode){
+        let textVal = $(this).val();
+        $("ul").append("<li><span><i class = 'fa fa-trash'></i></span>" + textVal +"</li>");
+        $(this).val("");  
+    }
+});
+
+$(".fa-plus").click(function(event){
+    $("input[type='text']").slideToggle(500);
+});
